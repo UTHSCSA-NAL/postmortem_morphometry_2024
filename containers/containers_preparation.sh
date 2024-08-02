0da6e9e4f4a40171921905d06fbdfedf17681d6e
nhonnorat <nicolas.honnorat@numericable.fr>
2024-08-02 13:27:45 -0500

    Add files via upload

---
+++ containers/containers_preparation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+echo "BUILDING CONTAINERS"
+singularity build --fakeroot ants.sif ants.def
+
+singularity build --fakeroot ants_c3d.sif ants_c3d.def
+
+singularity build --fakeroot fsl.sif fsl.def
+
+singularity build --fakeroot naonlm.sif naonlm.def
+
+singularity build --fakeroot python.sif python.def
+
+singularity build --fakeroot python_DeepLearning.sif python_DeepLearning.def 
+
+
+
+echo "CHECKING CONTAINERS"
+singularity exec ants.sif /ants/antsRegistration -h
+
+singularity exec ants_c3d.sif /ants/antsRegistration -h
+singularity exec ants_c3d.sif /c3d/c3d -h
+
+singularity exec fsl.sif /fsl/fsl5.0-fslmaths -h
+
+singularity exec naonlm.sif /naonlm3d/naonlm3d -h
+
+singularity exec python.sif python --version
+
+singularity exec python_DeepLearning.sif python -c "import tensorflow as tf;print(tf.__version__)"
+singularity exec python_DeepLearning.sif python -c "import keras; print(keras.__version__)"
+
+
+
+
